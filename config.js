a36b54de1a6ff8f185bc5819ae8d77147e94ced6
saduofc <sadumdofc@gmail.com>
2025-10-03 02:05:01 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,7 @@
+
+// Configuration File
+export default {
+  BOT_NAME: "QUEEN-SADU",
+  PREFIX: ".",
+  OWNER: ["94700000000"]
+};
---
+++ index.js
@@ -0,0 +1,7 @@
+
+// Main entry point
+import config from './config.js';
+import { loadPlugins } from './lib/utils.js';
+
+console.log("🚀 Bot Starting...");
+loadPlugins();
